63b99cfba8d68bf8cdbeb893316c3824be8cc46b
w <fw1746733@gmail.com>
2025-12-06 17:28:27 +0800

    wwwwww

---
+++ main/boards/my-custom-board/my_custom_board.cc
@@ -0,0 +1,16 @@
+#include "wifi_board.h"
+#include "codecs/es8311_audio_codec.h"
+#include "display/lcd_display.h"
+#include "application.h"
+#include "button.h"
+#include "config.h"
+#include "mcp_server.h"
+
+#include <esp_log.h>
+#include <driver/i2c_master.h>
+
+#define TAG "MyCustomBoard"
+
+class MyCustomBoard : public WifiBoard {};
+
+DECLARE_BOARD(MyCustomBoard);
